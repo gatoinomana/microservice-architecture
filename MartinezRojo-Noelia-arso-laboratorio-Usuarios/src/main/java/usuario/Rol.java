32f9a5a464341db8cc12522a78953f24cde183b8
Noelia Martínez Rojo <noelia.martinez32@um.es>
2020-06-22 13:18:05 +0200

    XML Schema y clases JAXB de proyecto Usuarios.

---
+++ MartinezRojo-Noelia-arso-laboratorio-Usuarios/src/main/java/usuario/Rol.java
@@ -0,0 +1,58 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2020.06.22 a las 01:16:53 PM CEST 
+//
+
+
+package usuario;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para rol.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="rol">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="profesor"/>
+ *     &lt;enumeration value="estudiante"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "rol")
+@XmlEnum
+public enum Rol {
+
+    @XmlEnumValue("profesor")
+    PROFESOR("profesor"),
+    @XmlEnumValue("estudiante")
+    ESTUDIANTE("estudiante");
+    private final String value;
+
+    Rol(String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public static Rol fromValue(String v) {
+        for (Rol c: Rol.values()) {
+            if (c.value.equals(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
+
+}
---
+++ MartinezRojo-Noelia-arso-laboratorio-Usuarios/src/main/java/usuario/Usuario.java
@@ -0,0 +1,121 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2020.06.22 a las 01:16:53 PM CEST 
+//
+
+
+package usuario;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;attribute name="nombre" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="email" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="rol" use="required" type="{http://www.example.org/usuario}rol" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+@XmlRootElement(name = "usuario")
+public class Usuario {
+
+    @XmlAttribute(name = "nombre", required = true)
+    protected String nombre;
+    @XmlAttribute(name = "email", required = true)
+    protected String email;
+    @XmlAttribute(name = "rol", required = true)
+    protected Rol rol;
+
+    /**
+     * Obtiene el valor de la propiedad nombre.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Define el valor de la propiedad nombre.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNombre(String value) {
+        this.nombre = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad email.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Define el valor de la propiedad email.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEmail(String value) {
+        this.email = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad rol.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Rol }
+     *     
+     */
+    public Rol getRol() {
+        return rol;
+    }
+
+    /**
+     * Define el valor de la propiedad rol.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Rol }
+     *     
+     */
+    public void setRol(Rol value) {
+        this.rol = value;
+    }
+
+}
